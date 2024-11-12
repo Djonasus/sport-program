bd69f3004dc1f708d3d5c824f0f7a0f0ff57c164
Mr.D <danil36rusdanil@mial.ru>
2024-11-12 11:28:59 +0300

    just commit

---
+++ SportProgam/src/main/java/com/example/SportProgam/SportProgamApplication.java
@@ -0,0 +1,13 @@
+package com.example.SportProgam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportProgamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportProgamApplication.class, args);
+	}
+
+}
---
+++ SportProgam/src/test/java/com/example/SportProgam/SportProgamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SportProgam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportProgamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
