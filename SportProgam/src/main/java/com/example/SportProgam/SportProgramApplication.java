6279d99d6cda9f4adc0a7edc07e47fcb543575dc
Kodi <hirofool502@gmail.com>
2024-11-12 12:09:11 +0300

    photo

--- SportProgam/src/main/java/com/example/SportProgam/SportProgramApplication.java
+++ SportProgam/src/main/java/com/example/SportProgam/SportProgramApplication.java
@@ -1,13 +1,15 @@
 package com.example.SportProgam;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition
 @SpringBootApplication
-public class SportProgamApplication {
+public class SportProgramApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SportProgamApplication.class, args);
+		SpringApplication.run(SportProgramApplication.class, args);
 	}
 
 }
