53dfbec34ad5d96bf8f27864f0ec7f562cbd5912
Djonasus <60808389+Djonasus@users.noreply.github.com>
2024-11-12 12:35:07 +0300

    Articles Component

--- source-frontend/src/screens/MainPage.js
+++ source-frontend/src/screens/MainPage.js
@@ -1,5 +1,12 @@
-const MainPage = () => {
+import { Container } from "react-bootstrap";
+import Articles from "../components/Articles";
 
+const MainPage = () => {
+    return (
+        <Container>
+            <Articles />
+        </Container>
+    )
 }
 
 export default MainPage;
