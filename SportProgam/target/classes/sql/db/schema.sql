620e81fc8d4e2101fcd09168c12d512e4608c1af
Da1ryman <asdfqw321244@gmail.com>
2024-11-26 12:08:10 +0300

    sport

---
+++ SportProgam/target/classes/sql/db/schema.sql
@@ -0,0 +1,34 @@
+-- Создание таблицы role
+CREATE TABLE IF NOT EXISTS public.role (
+    role_id SERIAL PRIMARY KEY,
+    role_name VARCHAR(255) UNIQUE NOT NULL
+);
+
+-- Создание таблицы users
+CREATE TABLE IF NOT EXISTS public.users (
+    user_id SERIAL PRIMARY KEY,
+    login VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES public.role(role_id)
+);
+
+-- Создание таблицы news
+CREATE TABLE IF NOT EXISTS public.news (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,  -- Изменено на DATE
+    url TEXT NOT NULL,
+    body TEXT NOT NULL,
+    author VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS public.coordinate (
+    coordinate_id SERIAL PRIMARY KEY,
+    x double precision  NOT NULL,
+    y double precision  NOT NULL,
+    description VARCHAR(255),
+    type VARCHAR(50)
+);
